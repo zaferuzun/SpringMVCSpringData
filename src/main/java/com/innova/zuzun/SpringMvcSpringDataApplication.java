d520f6b615c3eb55d2dad4b9876146d81b96f283
zaferuzun <uznzafer@gmail.com>
2022-02-05 16:41:44 +0300

    Initial commit

---
+++ src/main/java/com/innova/zuzun/SpringMvcSpringDataApplication.java
@@ -0,0 +1,13 @@
+package com.innova.zuzun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcSpringDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcSpringDataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/innova/zuzun/SpringMvcSpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innova.zuzun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcSpringDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
